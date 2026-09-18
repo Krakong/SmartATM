6fc1db974da89db95ea5ed877af9c99531763ea5
Krakong <chandan.su.parate@gmail.com>
2026-09-18 17:53:28 +0530

    Delete AirplaneSeatReservation directory

--- AirplaneSeatReservation/booking/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from .models import Flight,Seat,Booking
-admin.site.register([Flight,Seat,Booking])
--- AirplaneSeatReservation/booking/urls.py
+++
@@ -1,3 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[path('',views.home,name='home'),path('flight/<int:flight_id>/',views.flight_detail,name='flight_detail'),path('book/<int:seat_id>/',views.book_seat,name='book_seat'),path('my-bookings/',views.my_bookings,name='my_bookings'),path('login/',views.login_view,name='login'),path('logout/',views.logout_view,name='logout')]
